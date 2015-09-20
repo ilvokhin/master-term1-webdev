9b1d089e1efb5a0bf647a2479484afb4cb61e3a9
Dima Ilvokhin <r3tsky@gmail.com>
2015-09-20 13:15:22 +0000

    Added local cache with indexedDB
    If CouchDB response time is longer timeout, the data will taken
    from local indexedDB storage. If data from CouchDB received
    successfully, local store is updated.

--- js/simple_client.js
+++ js/simple_client.js
@@ -1,12 +1,107 @@
-function makeHttpRequest(type, path, data = '') {
+
+// server db
+function CouchDbViewer(dbName) {
+  this.dbName = dbName;
+  this.requestPath = '/' + this.dbName + '/_all_docs';
+}
+
+CouchDbViewer.prototype.getData = function (timeoutCallback) {
   var request = new XMLHttpRequest();
-  request.open(type, path, false);
+  request.ontimeout = timeoutCallback;
+
+  request.open('GET', this.requestPath, false);
+  //request.timeout = 1;
   request.send(data);
 
-  return request;
+  if( request.status != 200 ) {
+    console.log('CouchDB request failed: ' + request.statusText);
+  }
+
+  return request.responseText;
 }
- 
-function makeTable(parentElem, rawCouchData) {
+
+// local data storage
+function LocalCache(dbName) {
+  console.log('LocalCache: make local cache');
+  this.dbName = dbName;
+  this.indexedDb = window.indexedDB;
+  this.cacheKey = 0;
+}
+
+LocalCache.prototype.makeStoreName = function() {
+  return this.dbName + 'Store';
+}
+
+LocalCache.prototype.logError = function (err) {
+  console.log('LocalCache error: ' + err);
+}
+
+LocalCache.prototype.access = function (accessCallback) {
+  var request = indexedDB.open(this.dbName, 1);
+
+  request.onerror = this.logError;
+
+  request.onsuccess = function () {
+    accessCallback(request.result);
+    console.log('LocalCache: successfull access');
+  }
+
+  var storeName = this.makeStoreName();
+  request.onupgradeneeded = function (e) {
+    e.currentTarget.result.createObjectStore(storeName, {keyPath: 'key'});
+    console.log('LocalCache: new store maked');
+  }
+}
+
+LocalCache.prototype.get = function (successCallback) {
+  var storeName = this.makeStoreName();
+  var cacheKey = this.cacheKey;
+  this.access(function(db) {
+    var request = db.transaction([storeName], 'readonly').objectStore(storeName).get(cacheKey);
+    request.onerror = this.logError;
+    request.onsuccess = function() {
+      successCallback(request.result.val);
+    }
+  });
+}
+
+LocalCache.prototype.put = function (data) {
+  var storeName = this.makeStoreName();
+  var cacheKey = this.cacheKey;
+  this.access(function(db) {
+    var request = db.transaction([storeName], 'readwrite').objectStore(storeName).put({key: cacheKey, val: data});
+    request.onerror = this.logError;
+  });
+}
+
+// local + server 
+function CouchDbCache(dbName, makeTableCallback) {
+  this.couchDb = new CouchDbViewer(dbName);
+  this.localCache = new LocalCache(dbName);
+  this.makeTableCallback = makeTableCallback;
+}
+
+CouchDbCache.prototype.getData = function() {
+  console.log('Get cached data from db: ' + this.couchDb.dbName);
+
+  localCache = this.localCache;
+  makeTable = this.makeTableCallback;
+
+  var data = this.couchDb.getData(function() {
+    console.log('Timeout occurs should use cache');
+    localCache.get(makeTable);
+  });
+
+  if( data ) {
+    this.localCache.put(data);
+    this.makeTableCallback(data);
+  }
+
+  return data;
+}
+
+function makeTable(rawCouchData) {
+  var parentElem = 'db-table';
   var parsedCouchData = JSON.parse(rawCouchData);
 
   if( !parsedCouchData.total_rows ) {
@@ -40,7 +135,7 @@ function makeTable(parentElem, rawCouchData) {
   document.getElementById(parentElem).appendChild(table);
 }
 
-request = makeHttpRequest('GET', '/test/_all_docs');
-console.log(request.responseText);
-
-makeTable('db-table', request.responseText);
+// make request
+couchDb = new CouchDbCache('test', makeTable);
+var data = couchDb.getData();
+console.log(data);
